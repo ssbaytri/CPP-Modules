94e52068b1d537948d2c5619abdc0e8ebcfda528
ssbaytri <azjoker333@gmail.com>
2025-07-20 21:40:13 +0100

    ex03

--- CPP Module 03/ex03/main.cpp
+++ CPP Module 03/ex03/main.cpp
@@ -1,20 +1,19 @@
-#include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
 
 int main() {
-    FragTrap frodo("Frodo");
+    DiamondTrap d("Heisenberg");
 
-    frodo.attack("Orc");
-    frodo.takeDamage(25);
-    frodo.beRepaired(15);
-    frodo.highFivesGuys();
+    d.attack("Jesse");
+    d.takeDamage(40);
+    d.beRepaired(20);
+    d.whoAmI();
 
-    for (int i = 0; i < 5; ++i)
-        frodo.attack("Orc");
+    DiamondTrap copy(d);
+    copy.whoAmI();
 
-    frodo.takeDamage(100);
-    frodo.attack("james");
-    frodo.beRepaired(20);
-    frodo.highFivesGuys();
+    DiamondTrap assigned("Temporary");
+    assigned = d;
+    assigned.whoAmI();
 
     return 0;
 }
