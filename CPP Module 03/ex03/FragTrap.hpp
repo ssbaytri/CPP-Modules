4c903c5efbbcffd23ca5b4b0ff3ae7e884399a24
ssbaytri <azjoker333@gmail.com>
2025-07-20 14:20:08 +0100

    TBC

---
+++ CPP Module 03/ex03/FragTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <string>
+
+class FragTrap : public ClapTrap
+{
+	public:
+		FragTrap(const std::string& name);
+		FragTrap(const FragTrap& other);
+		FragTrap& operator=(const FragTrap& other);
+		~FragTrap();
+
+		void attack(const std::string& target);
+
+		void highFivesGuys();
+};
+
+#endif
