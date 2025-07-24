485c53ea319062f1e6a5fd3b324d220fd539afc7
$ubZ3r0 <106231563+ssbaytri@users.noreply.github.com>
2025-07-24 18:44:29 +0100

    ex00

--- CPP Module 04/ex00/main.cpp
+++ CPP Module 04/ex00/main.cpp
@@ -1,6 +1,8 @@
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 #include <iostream>
 
 int main() {
@@ -12,5 +14,22 @@ int main() {
   i->makeSound(); // will output the cat sound!
   j->makeSound();
   meta->makeSound();
+
+  delete meta;
+  delete j;
+  delete i;
+
+  // const WrongAnimal *meta = new WrongAnimal();
+  // const WrongAnimal *cat = new WrongCat();
+
+  // std::cout << meta->getType() << std::endl;
+  // std::cout << cat->getType() << std::endl;
+
+  // meta->makeSound();
+  // cat->makeSound();
+
+  // delete meta;
+  // delete cat;
+
   return 0;
 }
